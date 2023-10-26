ef9908505f050d1c1ef35e45d4e7b86e45f64c6a
Captone Habiyaremye <hcaptone@gmail.com>
2023-10-25 21:08:15 -0600

    DL init

--- src/config/engine.ts
+++ src/config/engine.ts
@@ -5,12 +5,16 @@ export const EngineModes: Record<string, Engine> = {
     // leetcode
     host: 'leetcode',
     input_question: ['__next', 'ssg__qd-splitter-primary-w', '[data-track-load="description_content"]'],
+    input_question_alt: ['__next', 'qd-content', '[data-track-load="description_content"]'],
     input_topics: ['__next', 'ssg__qd-splitter-primary-w'],
+    input_topics_alt: [],
     input_code: ['qd-content', 'ssg__qd-splitter-secondary-w', '[data-track-load="code_editor"]', 'view-lines'],
+    input_code_alt: ['qd-content', 'editor', '[data-track-load="code_editor"]'],
     submit_button: ['__next', 'console-submit-button', 'lc-home', 'submit__-6u9', 'ery7n2v0'],
     sidebarContainer: ['rhs'],
     appendContainerRight: ['ssg__qd-console-position--right', 'css-pwvbgl-CodeAreaContainer'],
     appendContainerLeft: ['ssg__qd-console-position--left', 'css-pwvbgl-CodeAreaContainer'],
+    appendContainerDL: ['editor'],
   },
   challenge: {
     // hacker-rank
@@ -22,5 +26,9 @@ export const EngineModes: Record<string, Engine> = {
     sidebarContainer: ['rhs'],
     appendContainerRight: ['hr-monaco-editor-statusbar'],
     appendContainerLeft: ['hr-monaco-custom-input-wrapper'],
+    input_question_alt: [],
+    input_topics_alt: [],
+    input_code_alt: [],
+    appendContainerDL: [],
   },
 };
--- src/interfaces/engine.ts
+++ src/interfaces/engine.ts
@@ -1,11 +1,15 @@
 export interface Engine {
   host: string;
   input_question: string[];
+  input_question_alt: string[];
   input_topics: string[];
+  input_topics_alt: string[];
   input_code: string[];
+  input_code_alt: string[];
   submit_button: string[];
   sidebarContainer: string[];
   appendContainerRight: string[];
   appendContainerLeft: string[];
+  appendContainerDL: string[];
   watchRouteChange?: (callback: () => Promise<void> | void) => void;
 }
