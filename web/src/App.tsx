55fac9d9d1a080e0e904ac9b94f053f412072875
Captone Habiyaremye <hcaptone@gmail.com>
2024-03-29 22:20:39 -0600

    Update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
       <CssBaseline />
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
